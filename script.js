7c9fe2b0de36dcf6ae6a804bf7cc62cfef523ab4
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2024-05-29 17:18:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -213,7 +213,7 @@ async function checkLocalStorage() {
             ],
             "processing": true,
             "responsive": true,
-            "order": [[13, 'asc'], [4, 'asc']],
+            "order": [[13, 'asc'],[4, 'asc'], [5, 'asc']],
             "dom": 'lBfrtip',
             "lengthMenu": [[10, 30, 70, 100, 150, 200, -1], [10, 30, 70, 100, 150, 200, "ทั้งหมด"]],
             "buttons": [
