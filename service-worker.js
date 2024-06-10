aab77e0e252d6f3ab79507cef93b8e85c58eb6da
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2024-06-10 09:35:39 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,31 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return Promise.all([
-                cache.add('/index.html'),
-                cache.add('/styles.css'),
-                cache.add('/script.js'),
-                cache.add('/nurse.png'),
-                cache.add('/nurse256.png'),
-                cache.add('/nurse512.png')
-            ]);
-        })
-    );
+// service-worker.js
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.error('Failed to cache resources:', error);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request).then(function(fetchResponse) {
-                return caches.open('my-cache').then(function(cache) {
-                    cache.put(event.request, fetchResponse.clone());
-                    return fetchResponse;
-                });
-            });
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
+
